49b16dd05c5202def8d138d09f6b372e898ce176
Abhishek Rai <abhishekrai@rathinfotech.com>
2025-02-14 16:51:08 +0530

    Added Longest Substring problem

--- Longest-substring.js
+++ Longest-substring.js
@@ -1,1 +1,26 @@
-console.log('Longest substring without repeating characters'); 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let longestArr = [];
+  let currentArr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (currentArr.includes(s[i])) {
+      if (currentArr.length > longestArr.length) {
+        longestArr = [...currentArr];
+      }
+      let index = currentArr.indexOf(s[i]);
+      currentArr = currentArr.slice(index + 1);
+    }
+
+    currentArr.push(s[i]);
+  }
+
+  if (currentArr.length > longestArr.length) {
+    longestArr = [...currentArr];
+  }
+
+  return longestArr.length;
+};
